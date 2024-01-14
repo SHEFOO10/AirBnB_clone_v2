cec6c020e013e446718586169eb7c68285886254
Mariem <mariem.ehab@ieee.org>
2024-01-14 10:43:39 -0800

    import of relationship update

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
