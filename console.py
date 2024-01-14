aafadc1a1bef3fefa6b04f10900e775931d96e2d
Mariem <mariem.ehab@ieee.org>
2024-01-14 06:14:44 -0800

    Task 2 update create in console and update base model

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,11 +118,25 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        args_splitted = args.split(" ")
+
+        if args_splitted[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        kwargs = {}
+        for param in args_splitted[1:]:
+            key, value = param.split("=")
+            if self.is_float(value):
+                kwargs[key] = float(value)
+            elif self.is_int(value):
+                kwargs[key] = int(value)
+            else:
+                kwargs[key] = value.strip('"\'')
+
+        new_instance = HBNBCommand.classes[args_splitted[0]](**kwargs)
+        storage.new(new_instance)
         print(new_instance.id)
         storage.save()
 
@@ -272,7 +286,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +294,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +333,23 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
+    def is_int(self, value):
+        """check if value is int"""
+        try:
+            int(value)
+            return True
+        except ValueError:
+            return False
+
+    def is_float(self, value):
+        """chack if value is float"""
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,23 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            else:
+                kwargs['updated_at'] =\
+                        datetime.strptime(kwargs['updated_at'],
+                                          '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+            else:
+                kwargs['created_at'] =\
+                        datetime.strptime(kwargs['created_at'],
+                                          '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
