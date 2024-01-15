c0fb2f6464f5ec209f62647dbfbdecabb81dc222
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-15 18:12:57 +0200

    add Delete method in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -65,3 +65,8 @@ class BaseModel():
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """ delete the current instance from the storage """
+        from models import storage
+        storage.delete(self)
