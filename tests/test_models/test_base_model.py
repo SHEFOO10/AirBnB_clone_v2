5dbf10fcbb847308174d7a857c79f8eae09f6618
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-17 16:54:02 +0200

    test skipif with test_save

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -39,6 +39,7 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', 'reason')
     def test_save(self):
         """ Testing save """
         i = BaseModel()
