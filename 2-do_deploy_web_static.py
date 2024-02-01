2ff7992ff7958b3b458f8af3e71be89d6cf7a4c3
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-01 18:08:25 +0200

    fix pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ import os.path as path
 env.user = "ubuntu"
 env.hosts = ["web-01.shefoo.tech", "web-02.shefoo.tech"]
 
+
 def do_pack():
     """
         compress web_static directory to tar archive
@@ -20,16 +21,16 @@ def do_pack():
     else:
         return None
 
+
 def do_deploy(archive_path):
     """ distributes an archive to your web servers """
-    if path.exists(archive_path) == False:
+    if path.exists(archive_path) is False:
         return False
-    print(f"{archive_path}")
     with cd('/tmp'):
         put(f"{archive_path}", f"{archive_path}")
         sudo("tar -xvzf {} -C /data/web_static/releases/".format(archive_path))
         sudo("rm -f {}".format(archive_path))
         sudo("rm -f /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/web_static /data/web_static/current")
-
-
+        sudo("ln -s /data/web_static/releases/web_static \
+/data/web_static/current")
+    return True
