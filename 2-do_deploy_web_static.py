e28f43f7614f729b21afe562e1f4739b5f36588c
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-01 19:54:06 +0200

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,14 +27,36 @@ def do_deploy(archive_path):
     if path.exists(archive_path) is False:
         return False
     try:
-        with cd('/tmp'):
-            archive_name = archive_path.split('/')[-1]
-            put(f"{archive_path}", f"{archive_name}")
-            sudo("tar -xvzf {} -C /data/web_static/releases/"
-                 .format(archive_name))
-            sudo("rm -f {}".format(archive_name))
-            sudo("rm -f /data/web_static/current")
-            sudo("ln -s /data/web_static/releases/web_static \
+        # upload archive to remote host
+        put(f"{archive_path}", "/tmp/")
+
+        # get archive name
+        archive_name = archive_path.split('/')[-1]
+
+        # set path for target directory
+        remote_path = "/data/web_static/releases/" + archive_name.split('.')[0]
+
+        # make sure target directory is present
+        sudo(f"mkdir -p {remote_path}")
+
+        # extract archive content
+        sudo("tar -xvzf /tmp/{} -C {}"
+             .format(archive_name, remote_path))
+
+        # delete archive from the remote server
+        sudo("rm -f /tmp/{}".format(archive_name))
+
+        # mv content of web_static_datetime/web_static to web_static_datetime
+        sudo(f"mv -f {remote_path}/web_static/* {remote_path}/")
+
+        # delete web_static directory
+        sudo(f"rm -r {remote_path}/web_static")
+
+        # remove existing link
+        sudo("rm -f /data/web_static/current")
+
+        # recreate link
+        sudo(f"ln -s /data/web_static/releases/{archive_name.split('.')[0]} \
 /data/web_static/current")
         return True
     except Exception:
