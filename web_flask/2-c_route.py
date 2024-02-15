749952584fff6957ed456f8790faeabcaf036198
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-15 23:15:07 +0200

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Initiate Flask Server """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/",
+           strict_slashes=False)
+def hello_world():
+    """ handle route to index"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb",
+           strict_slashes=False)
+def hbnb():
+    """ handle route to /hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>',
+           strict_slashes=False)
+def get_text(text):
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
