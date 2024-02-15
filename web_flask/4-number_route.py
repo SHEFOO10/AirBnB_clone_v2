d4591e97c56fcf61a1dcbd0ed03c8d800fb2beba
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-15 23:32:53 +0200

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Initiate Flask Server """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/",
+           strict_slashes=False)
+def hello_world():
+    """ handle route to index"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb",
+           strict_slashes=False)
+def hbnb():
+    """ handle route to /hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>',
+           strict_slashes=False)
+def get_text(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>',
+           strict_slashes=False)
+def get_text_2(text):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>',
+           strict_slashes=False)
+def get_int(n):
+    return str(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
