7eaae1ee88e52496e8f0671e048009bba3071b43
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-17 00:40:48 +0200

    escape Name and id

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def list_states():
     """ list states """
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    sorted_states = sorted(list(states.values()), key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 
